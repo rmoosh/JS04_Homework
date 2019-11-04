79035715f4a095563dd05d0c4b65da435ae24510
Reema <56271851+rmoosh@users.noreply.github.com>
2019-11-04 15:58:29 +0300

    ass 4

---
+++ ReemaAldowais/week4/js/HW.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.backgroundColor = "pink";
+    x.style.fontFamily= "Arial, sans-serif";
+    
+  
+  document.getElementById("nickName").innerHTML = "Rmoosh";
+  document.getElementById("homeTown").innerHTML= "Riyadh";
+  document.getElementById("favorites").innerHTML = "Capital City ";
+  
+    
+  }
